01aab7c3dfe93792794cec77e1d255e41f394129
irinasiur <drozdovairina10@gmail.com>
2023-05-26 21:24:30 +0300

    realized functions and added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+if __name__ == '__main__':
+    item1 = Item('bag', 900, 10)
+
+    # TastCase #1 Calculate_Total_Price
+
+
+def test_calculate_total_price():
+    item11 = Item('bag', 900, 10)
+    assert item11.calculate_total_price() == 9000
+    item2 = Item('doll', 50.50, 10)
+    assert item2.calculate_total_price() == 505.0
+    item3 = Item('cat', 100, 0)
+    assert item3.calculate_total_price() == 0
+
+
+def apply_discount() -> None:
+    assert item1.apply_discount() is None
+
